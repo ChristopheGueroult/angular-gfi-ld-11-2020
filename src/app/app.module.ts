d6b520d2286c3e17dcb1da6c70a84f397e97f5e8
christophe gueroult <christophegueroult@gmail.com>
2020-11-17 10:30:03 +0000

    end ui component, ng-content and ng-template

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,14 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { BrowserModule } from '@angular/platform-browser';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { LoginModule } from './login/login.module';
 import { CoreModule } from './core/core.module';
-import { SharedModule } from './shared/shared.module';
-import { UiModule } from './ui/ui.module';
 import { IconsModule } from './icons/icons.module';
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { LoginModule } from './login/login.module';
+import { UiModule } from './ui/ui.module';
+
+
 
 
 @NgModule({
@@ -23,8 +22,7 @@ import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
     CoreModule,
     UiModule,
     IconsModule,
-    NgbModule,
-    FontAwesomeModule
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/ui/components/ui/ui.component.ts
+++ src/app/ui/components/ui/ui.component.ts
@@ -1,15 +1,22 @@
 import { Component, OnInit } from '@angular/core';
 
+/**
+ * @description need to projet 5 elements with class header, class nav and class footer class icon-nav, class icon-close
+ */
 @Component({
   selector: 'app-ui',
   templateUrl: './ui.component.html',
   styleUrls: ['./ui.component.scss']
 })
 export class UiComponent implements OnInit {
-
+  public open = true;
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  public toggle(): void {
+    this.open = !this.open;
   }
 
 }
