09e6434e279534522c94864f182009dadae6ca60
christophe gueroult <christophegueroult@gmail.com>
2020-11-19 11:09:06 +0000

    pipe async and changeDetectorRef

--- src/app/orders/pages/page-list-orders/page-list-orders.component.ts
+++ src/app/orders/pages/page-list-orders/page-list-orders.component.ts
@@ -1,24 +1,34 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy, ChangeDetectorRef } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { OrdersService } from 'src/app/core/services/orders.service';
 import { Order } from 'src/app/core/models/order';
 import { StateOrder } from 'src/app/core/enums/state-order.enum';
+import { Observable, Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-page-list-orders',
   templateUrl: './page-list-orders.component.html',
   styleUrls: ['./page-list-orders.component.scss']
 })
-export class PageListOrdersComponent implements OnInit {
+export class PageListOrdersComponent implements OnInit, OnDestroy {
   public listHeaders: string[];
-  public collection: Order[];
+  // public collection: Order[];
+  public collection$: Observable<Order[]>;
   public states = Object.values(StateOrder);
+  private obs = new Observable((subsribers) => {
+    subsribers.next('ola les amigos');
+  });
+  private sub: Subscription;
   constructor(
     private route: ActivatedRoute,
-    private os: OrdersService
+    private os: OrdersService,
+    private cd: ChangeDetectorRef
   ) { }
 
   ngOnInit(): void {
+    this.sub = this.obs.subscribe((data) => {
+      console.log(data);
+    });
     this.listHeaders = [
       'Type',
       'Client',
@@ -31,9 +41,10 @@ export class PageListOrdersComponent implements OnInit {
     this.route.data.subscribe((param) => {
       // console.log(param);
     });
-    this.os.collection.subscribe((datas) => {
-      this.collection = datas;
-    });
+    // this.os.collection.subscribe((datas) => {
+    //   this.collection = datas;
+    // });
+    this.collection$ = this.os.collection;
   }
 
   public changeState(item: Order, event): void {
@@ -41,6 +52,11 @@ export class PageListOrdersComponent implements OnInit {
     this.os.changeState(item, state).subscribe((res) => {
       // tester les erreurs
       item.state = res.state;
+      this.cd.detectChanges();
     });
+  }
+
+  ngOnDestroy(): void {
+    this.sub.unsubscribe();
   }
 }
